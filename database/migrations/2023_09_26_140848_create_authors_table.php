b11c2148c72b062e005b44b82d0170edc1b73304
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-09-26 18:10:54 +0400

    feat - make migrations for book and author

---
+++ database/migrations/2023_09_26_140848_create_authors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('author_book', function (Blueprint $table) {
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('book_id');
+
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authors');
+    }
+};
