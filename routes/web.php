7ddc8112004de49fb88be6f64a2c054fb121de03
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-09-28 17:44:34 +0400

    feat - routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,26 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::get('/', 'show')->name('home');
     Route::post('/login', 'login')->name('login');
+    Route::get('/login', 'show')->name('login.show');
     Route::get('/dashboard', 'dashboard')->name('dashboard')->middleware('auth');
     Route::get('/logout', 'logout')->name('logout');
+});
+
+
+Route::prefix('/books')->controller(BookController::class)->middleware('auth')->group(function () {
+    Route::get('/', 'index')->name('books.index');
+    // Route::get('/create', 'create')->name('books.create');
+    // Route::post('/', 'store')->name('books.store');
+    Route::get('/{book}/edit', 'edit')->name('books.edit');
+    Route::put('/{book}', 'update')->name('books.update');
+    Route::delete('/{book}', 'destroy')->name('books.destroy');
+});
+
+Route::prefix('/authors')->controller(AuthorController::class)->middleware('auth')->group(function () {
+    Route::get('/', 'index')->name('authors.index');
+    // Route::get('/create', 'create')->name('authors.create');
+    // Route::post('/', 'store')->name('authors.store');
+    Route::get('/{author}/edit', 'edit')->name('authors.edit');
+    Route::put('/{author}', 'update')->name('authors.update');
+    Route::delete('/{author}', 'destroy')->name('authors.destroy');
 });
